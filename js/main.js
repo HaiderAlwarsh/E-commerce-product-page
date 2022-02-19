25a8b1f9a666dce5145236a92071ce4f60708af9
Haider Alwarsh <haideralwarsh11@gmail.com>
2022-02-20 00:20:41 +0300

    Finished slider section

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
 
-    $('.burger-close-icon').on('click', function(){
-        slideMenu();
+    let imageUrl = ['/imgs/image-product-1.jpg', '/imgs/image-product-2.jpg', '/imgs/image-product-3.jpg', '/imgs/image-product-4.jpg'];
+    let counter = 0;
+    
+    $(document).on('click', function(e){
+
+        let target = e.target.parentElement;
+
+        console.log(target);
+
+        if($(target) && $(target).hasClass('burger-close-icon')){
+            slideMenu();
+        
+        }else if($(target) && $(target).hasClass('main-section-productImages')){
+            imageSlider(e.target);
+        }
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -48,6 +48,30 @@ function slideMenu(){
 }
 
 
+// Change product image
+function imageSlider(target){
+
+    if($(target).hasClass('next-arrow')){
+        
+        counter ++;
+
+        if(counter == imageUrl.length ){
+            counter = 0;
+        }
+
+        $('.main-section-productImages').css('background-image', 'url(' +imageUrl[counter]+ ')');
+
+    }else if ($(target).hasClass('prev-arrow')){
+
+        counter --;
+
+        if(counter == -1){
+            counter = imageUrl.length - 1;
+        }
+
+        $('.main-section-productImages').css('background-image', 'url(' +imageUrl[counter]+ ')');                
+    }
+}
 
 
 })
