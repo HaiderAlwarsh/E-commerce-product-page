23d98aff86d3a7ed5cc9f1dad7059d59302bd2dc
Haider Alwarsh <haideralwarsh11@gmail.com>
2022-02-22 17:56:40 +0300

    add product list

--- js/main.js
+++ js/main.js
@@ -2,16 +2,42 @@ $(document).ready(function(){
 
     let imageUrl = ['/imgs/image-product-1.jpg', '/imgs/image-product-2.jpg', '/imgs/image-product-3.jpg', '/imgs/image-product-4.jpg'];
     let counter = 0;
+    let numOfProd = 0
     
     $(document).on('click', function(e){
 
         let target = e.target.parentElement;
-
+    
         if($(target) && $(target).hasClass('burger-close-icon')){
+
+            preventMultiClick('.burger-close-icon')
             slideMenu();
         
         }else if($(target) && $(target).hasClass('main-section-productImages')){
+
+            let className = e.target.className;
+            
+            preventMultiClick('.' + className)
             imageSlider(e.target);
+        
+        }else if ($(target) && $(target).hasClass('add-to-cart')){
+            let amount = parseInt($('.amount').text());
+            let cart = $('.cart');
+
+            if(amount > 0 ){
+                cart.addClass('active');
+                cart.attr('data-amount', amount)
+                
+                addProduct();
+            }
+
+        }else if($(target) && $(target).hasClass('product-count')){
+            CountQuantity(e.target);
+        
+        }else if($(target) && $(target).hasClass('cart')){
+            setTimeout(function(){
+                DisplayCart();
+            }, 200)
         }
     })
 
@@ -67,5 +93,53 @@ function imageSlider(target){
         $('.main-section-productImages').css('background-image', 'url(' +imageUrl[counter]+ ')');                
     }
 }
+
+// Prevent multi click
+function preventMultiClick(elmName){
+
+    $(elmName).css('pointer-events', 'none')
+
+    setTimeout(function(){
+        $(elmName).css('pointer-events', 'unset')
+        }, 400)
+}
+
+
+function CountQuantity(ele){
+    if($(ele).hasClass('minus')){
+        numOfProd --;
+
+        if(numOfProd <= 0 ){
+            numOfProd = 1;
+        }
+
+        $('.amount').text(numOfProd);
+    
+    }else if($(ele).hasClass('plus')){
+        numOfProd ++;
+        $('.amount').text(numOfProd);
+    }
+}
+
+
+//Display product in the cart and all details
+function DisplayCart(){
+
+    let cartList = '<div class="cart-list"><header><h3>Cart</h3></header><section class="cart-list-body"><p>Your cart is empty</p></section></div>';
+
+    if($('body').find('.cart-list').length != 1){
+        $('.main-div').after(cartList)
+        
+    }else {
+        $('body .cart-list').remove()
+    }
+
+}
+
+
+function addProduct(){
+    let addProduct = '';
+}
+
 
 })
